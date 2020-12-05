850900bfdd7fa0136e0c13350d45f2f4b3b23806
Jorge <grusite@gmail.com>
2020-12-05 16:00:35 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello world!!!! {{ name }}</h1>
+</template>
+
+<script>
+  export default {
+      data() {
+          return {
+              name: "Jorge"
+          }
+      }
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
