92a9961fc02e3c3953efff03c47742abbe00d73f
Jorge <grusite@gmail.com>
2020-12-19 01:55:37 +0100

    handling errors

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,18 +1,43 @@
 export default function(context, inject) {
   const appId = '2PTSPQFW4Y'
   const apiKey = 'aa3d4392845e89b4a2b2e37ac47a848d'
+  const headers = {
+    'X-Algolia-API-Key': apiKey,
+    'X-Algolia-Application-Id': appId,
+  }
 
   inject('dataApi', {
     getHome,
   })
   async function getHome(homeId) {
-    const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
-      headers: {
-        'X-Algolia-API-Key': apiKey,
-        'X-Algolia-Application-Id': appId,
-      },
-    })
+    try {
+      return unwrap(
+        await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
+          headers,
+        })
+      )
+    } catch (error) {
+      return getErrorResponse(error)
+    }
+  }
+
+  async function unwrap(response) {
     const json = await response.json()
-    return json
+    const { ok, status, statusText } = response
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    }
+  }
+
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {},
+    }
   }
 }
